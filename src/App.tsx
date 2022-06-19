0fafd4448a288cb71f1e5145bd62f8cf0c679e33
likui628 <likui.it@gmail.com>
2022-06-19 17:27:51 +0800

    chore: TodoMVC style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input
+          class="new-todo"
+          placeholder="What needs to be done?"
+          autofocus
+        />
       </header>
-    </div>
+      <section class="main" style="display: block;">
+        <input id="toggle-all" class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <li data-id="1655628735268" class="">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>todo</label>
+              <button class="destroy"></button>
+            </div>
+          </li>
+          <li data-id="1655628738641" class="completed">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>done</label>
+              <button class="destroy"></button>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <footer class="footer" style="display: block;">
+        <span class="todo-count">
+          <strong>1</strong> item left
+        </span>
+        <ul class="filters">
+          <li>
+            <a href="#/" class="selected">
+              All
+            </a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        <button class="clear-completed" style="display: block;">
+          Clear completed
+        </button>
+      </footer>
+    </section>
   );
 };
 
